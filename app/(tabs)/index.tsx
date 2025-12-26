27149231d475251bb40a21fb0f444d3f0bfaa5d4
geeksamarth <samarthagrawal1804@gmail.com>
2025-12-26 18:31:55 +0530

    add: home screen with header component and its styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,27 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import Header from "@/components/Header";
+import useTheme from "@/hooks/useTheme";
+import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import { StatusBar, StyleSheet, Text } from "react-native";
+import { StatusBar, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
 const index = () => {
+  const { toggleDarkMode, colors } = useTheme();
+  const homeStyles = createHomeStyles(colors);
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <Text>index</Text>
-    </SafeAreaView>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homeStyles.container}
+    >
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Header />
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>Toggle the Mode</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
 export default index;
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    minHeight: "100%",
-    backgroundColor: "#faedcd",
-  },
-});
